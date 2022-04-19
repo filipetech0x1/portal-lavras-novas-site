72ecdbbf8f97b97cc9953ce7aa0f61a854bf0593
filip1z <filipehermecunha@gmail.com>
2022-04-19 00:21:44 -0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <style>
+*{
+    margin: 0;
+    padding: 0;
+}
+a{
+    color: #fff;
+    text-decoration: none;
+}
+a:hover{
+    opacity: 0.7;
+}
+.logo{
+    font-size: 24px;
+    text-transform: uppercase;
+    letter-spacing: 4px;
+}
+nav{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background: #054030;
+    height: 8vh;
+}
+main{
+    background: url() no repeat center center;
+    background-size: cover;
+    height:92vh;
+}
+.nav-list{
+    list-style: none;
+    display: flex;
+}
+.nav-list{
+    letter-spacing: 3px;
+    margin-left: 32px;
+}
+.mobile-menu{
+    display: none;
+    cursor: pointer;
+}
+
+.mobile-menu div{
+    width:32px;
+    height: 2px;
+    background:#fff;
+    margin:8px;
+}
+@media (max-width: 999px){
+    
+    body{
+        overflow-x: hidden;
+    }
+    .nav-list{
+        position: absolute;
+        top: 8vh;
+        right: 0;
+        width: 50vw;
+        height: 92vh;
+        background:#054030;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-around;
+        transform: translateX(100%);
+        transition: transform 0.3s ease-in;
+    }
+    .nav-list li{
+        margin-left: 0;
+        opacity: 0;
+    }
+    .mobile-menu{
+        display: block;
+    }
+    .nav-list.active {
+        transform: translateX(0);
+    }
+
+    @keyframes navLinkFade {
+        from{
+            opacity: 0;
+            transform: translateX(50px);
+        }
+        to{
+            opacity: 1;
+            transform: translateX(0);
+        }
+    }
+}
+    </style>
+    <title>Portal Lavras Novas</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a class="logo" href="">Portal Lavras Novas</a>
+            <div class="mobile-menu">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+            <section class="menu">
+                <ul class="nav-list">
+                    <li><a href="{{ route('cachoeiras')}}">Cachoeiras</a></li>
+                    <li> <a href="{{ route('cultura')}}">Cultura</a></li>
+                    <li><a href="{{ route('gastronomia')}}">Gastronomia</a></li>
+                    <li> <a href="{{ route('hospedagem')}}">Hospedagem</a></li>
+                    <li><a href="{{ route('loja')}}">Loja</a></li>
+                </ul>
+            </section>
+        </nav>
+        
+    </header>
+    <main></main>
+    <script type="text/javascript">
+    class MobileNavbar {
+
+        constructor(mobileMenu, navList, navLinks){
+            this.mobileMenu = document.querySelector(mobileMenu);
+            this.navList = document.querySelector(navList);
+            this.navLinks = document.querySelector(navLinks);
+            this.activeClass = "active";
+
+            this.handleClick = this.handleClick.bind(this);
+        }
+        animateLinks(){
+            this.navLinks.array.forEach(link => {
+                console.log(index);
+                link.style.animation
+                ?(link.style.animation = "")
+                : (link.style.animation = `navLinkFade 0.5s ease forwards 0.3s`);
+            });
+            
+        }
+
+        handleClick(){
+            this.navList.classList.toggle(this.activeClass);  
+            this.animateLinks();
+        }
+
+        addClickEvent(){
+            this.mobileMenu.addEventListener("click", this.handleClick);
+        }
+
+        init(){
+            if(this.mobileMenu){
+                this.addClickEvent();
+            }
+            return this;
+        }
+    }
+    
+    const mobileNavbar = new MobileNavbar(
+        ".mobile-menu",
+        ".nav-list",
+        ".nav-list li",
+    );
+    mobileNavbar.init();
+    </script>
+    
+    
+    <footer>
+
+    </footer>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+/*
+Route::get('/', function () {
+    return view('welcome');
+});
+*/
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::view('/loja', 'loja' )->name('loja');
+
+Route::view('/hospedagem', 'hospedagem' )->name('hospedagem');
+
+Route::view('/cachoeiras', 'cachoeiras' )->name('cachoeiras');
+
+Route::view('/cultura', 'cultura')->name('cultura');
+
+Route::view('/gastronomia', 'gastronomia')->name('gastronomia');
